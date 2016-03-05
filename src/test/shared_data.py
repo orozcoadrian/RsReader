576dda66a8444b979d27ad29300120a8e5c7c28e
orozcoadrian <orozcoadrian@gmail.com>
2016-03-04 23:59:05 -0500

    pg 182

--- src/test/shared_data.py
+++ src/test/shared_data.py
@@ -1,3 +1,8 @@
-__all__ = ['printed_items']
-printed_items = \
-    """Wed, 05 Dec 2007 05:00:00 -0000: xkcd.com: Python\nMon, 03 Dec 2007 05:00:00 -0000: xkcd.com: Far Away"""
+import os
+
+__all__ = ['expected_items', 'printed_items']
+expected_items = [
+    "Wed, 05 Dec 2007 05:00:00 -0000: xkcd.com: Python",
+    "Mon, 03 Dec 2007 05:00:00 -0000: xkcd.com: Far Away"
+]
+printed_items = os.linesep.join(expected_items)
--- src/test/test_application.py
+++ src/test/test_application.py
@@ -3,20 +3,17 @@ from nose.tools import *
 from rsreader.application import RSReader
 from test.shared_data import *
 
+items = [{'date': "Wed, 05 Dec 2007 05:00:00 -0000",
+          'title': "Python"},
+         {'date': "Mon, 03 Dec 2007 05:00:00 -0000",
+          'title': "Far Away"}]
+feed = {'feed': {'title': "xkcd.com"}, 'entries': items}
+
 def test_listing_from_item():
-    expected_line = """Wed, 05 Dec 2007 05:00:00 -0000: xkcd.com: Python"""
-    item = {'date': "Wed, 05 Dec 2007 05:00:00 -0000",
-            'title': "Python"}
-    feed = {'feed': {'title': "xkcd.com"}}
-    computed_line = RSReader().listing_from_item(feed, item)
-    assert_equals(expected_line, computed_line)
+    computed_line = RSReader().listing_from_item(feed, items[0])
+    assert_equals(expected_items[0], computed_line)
 
 
 def test_feed_listing():
-    items = [{'date': "Wed, 05 Dec 2007 05:00:00 -0000",
-              'title': "Python"},
-             {'date': "Mon, 03 Dec 2007 05:00:00 -0000",
-              'title': "Far Away"}]
-    feed = {'feed': {'title': "xkcd.com"}, 'entries': items}
     computed_line = RSReader().feed_listing(feed)
     assert_equals(printed_items, computed_line)
