ee7e0325ae4c5d7ada5b00dfbabaa502da57e6ea
orozcoadrian <orozcoadrian@gmail.com>
2016-03-04 21:56:09 -0500

    pg 156

---
+++ src/rsreader/application.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    xkcd_items = \
+        """Wed, 05 Dec 2007 05:00:00 -0000: xdkc.com: Python
+        Mon, 03 Dec 2007 05:00:00 -000: xkcd.com: Far Away"""
+    print(xkcd_items)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ src/test/test_application.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+import sys
+from io import StringIO
+
+from rsreader.application import main
+
+
+class AcceptanceTests(TestCase):
+    def setUp(self):
+        self.old_value_of_stdout = sys.stdout
+        sys.stdout = StringIO()
+        self.old_value_of_argv = sys.argv
+
+    def tearDown(self):
+        sys.stdout = self.old_value_of_stdout
+        sys.argv = self.old_value_of_argv
+
+    def test_should_get_one_URL_and_print_output(self):
+        printed_items = """Wed, 05 Dec 2007 05:00:00 -0000: xdkc.com: Python
+    Mon, 03 Dec 2007 05:00:00 -000: xkcd.com: Far Away"""
+
+        sys.argv = ["unused_prog_name", "xkcd.rss.xml"]
+        main()
+        self.assertEquals(printed_items + "\n",
+                          sys.stdout.getvalue())
+
+    def test_no_urls_should_print_nothing(self):
+        sys.argv = ["unused_prog_name"]
+        main()
+        self.assertEquals("", sys.stdout.getvalue())
